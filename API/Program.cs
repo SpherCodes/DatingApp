44c27ec1550554e2bc3c6a1f02bb1a091c54335b
SpherCode <sphersphiwe24@gmail.com>
2025-02-23 14:32:40 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +11,13 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200" , "https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
