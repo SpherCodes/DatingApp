9f2004147511b5341089c30352e545b3d5ee81bc
SpherCode <sphersphiwe24@gmail.com>
2025-03-04 07:09:25 +0200

    End of Section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
+    {
+        [HttpPost("register")] // POST: api/account/register
+        public async Task<ActionResult<UserDto>> Register(RegisterDTO registerDTO)
+        {
+            if (await UserExists(registerDTO.UserName)) return BadRequest("Username is taken");
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDTO.UserName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = tokenService.CreateToken(user)
+            };
+        }
+        [HttpPost("login")] // POST: api/account/login
+        public async Task<ActionResult<UserDto>> Login(LoginDTO loginDto)
+        {
+            var user = await context.Users
+                .FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = tokenService.CreateToken(user)
+            };
+        }
+
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
+        }
+    }
+}
